0efdb52ab5fc8662497b786629e66576bd52533e
Weimar Ustarez <weimar.ustarezrivera@gmail.com>
2023-06-18 21:37:42 -0400

    probando una solución

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,13 @@ Route::get('/', function () {
 use App\Http\Controllers\PersonaController;
 
 Route::get('/personas', [PersonaController::class, 'index'])->name('personas.index');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
